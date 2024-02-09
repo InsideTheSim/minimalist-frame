d29b852f2c01663871154189da6c01ca9a5c6b9a
InsideTheSimulation <insidethesimulation@pm.me>
2024-02-08 20:23:30 -0500

    move API route into API directory

--- api/index.mjs
+++ api/index.mjs
@@ -1,25 +1,3 @@
-import http from "http"
-
-const hostname = "127.0.0.1"
-const port = 80
-
-const home = `<html lang="en">
-<head>
-  <title>Minimalist Frame - A</title>
-  <meta property="fc:frame" content="vNext" />
-  <meta
-    property="fc:frame:image"
-    content="https://minimalist-frame.vercel.app/img/a.png"
-  />
-  <meta property="fc:frame:button:1" content="Go to B" />
-  <meta property="fc:frame:button:2" content="Go to C" />
-  <meta property="fc:frame:post_url" content="https://minimalist-frame.vercel.app" />
-</head>
-<body>
-  <h1>Minimalist Frame - A</h1>
-</body>
-</html>`
-
 const b = `<html lang="en">
 <head>
   <title>Minimalist Frame - B</title>
@@ -49,7 +27,7 @@ const c = `<html lang="en">
 </html>
 `
 
-const server = http.createServer((req, res) => {
+export default function handler(req, res) {
   if (req.method === "POST") {
     if (req.url === "/b.html") {
       res.statusCode = 200
@@ -64,9 +42,4 @@ const server = http.createServer((req, res) => {
       return
     }
   }
-  res.statusCode = 200
-  res.setHeader("Content-Type", "text/html")
-  res.end(home)
-})
-
-server.listen()
+}
