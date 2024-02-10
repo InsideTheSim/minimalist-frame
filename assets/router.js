eb90a238648ed9257a5233807340907619231b86
InsideTheSimulation <insidethesimulation@pm.me>
2024-02-10 11:58:04 -0500

    update createRouter to export callable function

--- assets/router.js
+++ assets/router.js
@@ -1,18 +1,20 @@
 export default function createRouter(req, res, target1, target2) {
-  if (req.method === "POST") {
-    const data = req.body.untrustedData
-    if (!data) {
-      res.statusCode = 400
-      return res.send("Invalid request")
+  return function () {
+    if (req.method === "POST") {
+      const data = req.body.untrustedData
+      if (!data) {
+        res.statusCode = 400
+        return res.send("Invalid request")
+      }
+      res.statusCode = 200
+      res.setHeader("Content-Type", "text/html")
+      if (data.buttonIndex === 1) {
+        return res.send(target1)
+      }
+      if (data.buttonIndex === 2) {
+        return res.send(target2)
+      }
     }
-    res.statusCode = 200
-    res.setHeader("Content-Type", "text/html")
-    if (data.buttonIndex === 1) {
-      return res.send(target1)
-    }
-    if (data.buttonIndex === 2) {
-      return res.send(target2)
-    }
+    return res.status(400).end()
   }
-  return res.status(400).end()
 }
